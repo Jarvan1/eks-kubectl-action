bacf1fad0bd4e99de46a41fc1701187e1fc90c75
xinming <xinming.xxm@shopastro-inc.com>
2022-06-24 15:42:13 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,11 +21,11 @@ if [ -n "${INPUT_AWS_REGION:-}" ]; then
   export AWS_DEFAULT_REGION="${INPUT_AWS_REGION}"
 fi
 
-if [ -n "${INPUT_MANIFESTS_FILE:-}" ] && [ -n "${INPUT_IMAGE:-}" ]; then
-  # export AWS_DEFAULT_REGION="${INPUT_AWS_REGION}"
-  sed -i "s#image:.*#image: ${INPUT_IMAGE}#g" ${INPUT_MANIFESTS_FILE}
-  kubectl apply -f ${INPUT_MANIFESTS_FILE}
-fi
+# if [ -n "${INPUT_MANIFESTS_FILE:-}" ] && [ -n "${INPUT_IMAGE:-}" ]; then
+#   # export AWS_DEFAULT_REGION="${INPUT_AWS_REGION}"
+#   # sed -i "s#image:.*#image: ${INPUT_IMAGE}#g" ${INPUT_MANIFESTS_FILE}
+#   # kubectl apply -f ${INPUT_MANIFESTS_FILE}
+# fi
 
 echo "aws version"
 
@@ -38,7 +38,8 @@ if [ -n "${INPUT_EKS_ROLE_ARN}" ]; then
 else 
   aws eks update-kubeconfig --name "${INPUT_CLUSTER_NAME}"
 fi
-
+echo "sed -i "s#image:.*#image: ${INPUT_IMAGE}#g" ${INPUT_MANIFESTS_FILE}"
+sed -i "s#image:.*#image: ${INPUT_IMAGE}#g" ${INPUT_MANIFESTS_FILE}
 debug "Starting kubectl collecting output"
 output=$( kubectl "$@" )
 debug "${output}"
